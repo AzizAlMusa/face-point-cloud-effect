4e031af1dc14df80048e7ef2c73ef2585c21d063
AzizAlmusa <abdulazizalmusa@gmail.com>
2021-05-03 04:10:07 +0300

    Completed 3D model with centered pivot and HSL editing

--- js/main.js
+++ js/main.js
@@ -1,79 +1,102 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 var renderer = new THREE.WebGLRenderer();
 var scan;
+var mesh;
+var pivot;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const light = new THREE.AmbientLight(0xffffff);
+
 scene.add(light);
 
+const directionalLight = new THREE.DirectionalLight(0xff0000, 40);
+directionalLight.castShadows = true;
+directionalLight.position.z = 0;
+directionalLight.position.y = 0;
+directionalLight.position.x = 20;
+//scene.add(directionalLight);
 
+const pointLight = new THREE.PointLight(0xf0ea76, 1, 200);
+pointLight.position.set(0, -5, 5);
+scene.add(pointLight);
+
+const pointLight2 = new THREE.PointLight(0x0000ff, 5, 200);
+pointLight2.position.set(-20, 20, 5);
+scene.add(pointLight2);
+
+const ball = new THREE.SphereGeometry(1, 32, 32);
+const ballMaterial = new THREE.MeshStandardMaterial({
+  color: 0xff0000,
+  wireframe: true
+});
+
+const ballMesh = new THREE.Mesh(ball, ballMaterial);
+//scene.add(ballMesh);
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
-/*
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = true;
-controls.minDistance = 40;
-controls.maxDistance = 40;
+controls.minDistance = 30;
+controls.maxDistance = 30;
 controls.maxPolarAngle = Math.PI / 2;
 controls.minPolarAngle = Math.PI / 2; // radians
-controls.minAzimuthAngle = -Math.PI/2; // radians
-controls.maxAzimuthAngle = Math.PI/2 ; // radians
+//controls.minAzimuthAngle = -Math.PI / 2; // radians
+//controls.maxAzimuthAngle = Math.PI / 2; // radians
 controls.rotateSpeed = 0.7;
 controls.enablePan = false;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 2.0;
-*/
+///////////////////MODEL/////////////////////////////
+
 var loader = new THREE.GLTFLoader();
 loader.load("model/scan2.glb", function (gltf) {
-    var scale = 1;
-    scan = gltf.scene.children[0];
-    //const armAxes = new THREE.AxesHelper(10 / scale);
-    //scan.rotation.set(Math.PI/2, 0, 0);
-    //scan.position.set(0,0,0);
-    scan.position.y = 0;
-    //arm.add(armAxes);
-    scan.scale.set(scale, scale, scale);
-    //arm.rotation.set(0, Math.PI / 2, Math.PI / 2);
+  var scale = 100;
+  mesh = gltf.scene.children[0];
+  mesh.geometry;
 
-    //arm.updateMatrix();
-    scan.material.color.setHex(0xa8ede5); //#D90B3E //0xfff77d
-    //scan.material = new THREE.MeshLambertMaterial( { color: 0xffffff} );
-    geometry = scan.geometry;
-    material = new THREE.PointsMaterial({ color: 0xa8ede5, size: 0.0045 });
-    material.blending = THREE.AdditiveBlending;
-    var mesh = new THREE.Points(geometry, material);
-    center = mesh.geometry.boundingSphere.center;
-    mesh.rotation.set(Math.PI/2, 0, 0);
+  //mesh.scale.set(scale, scale, scale);
+  let material = new THREE.PointsMaterial({ color: 0xaaaaaa, size: 0.0045 });
 
-    var sphere = new THREE.SphereGeometry(mesh.geometry.boundingSphere.radius, 32, 32);
-    
-    
-    sphereMesh = new THREE.Mesh(sphere, new THREE.MeshLambertMaterial( { color: 0xff0000} ));
-    sphereMesh.position.set(center);
-    console.log(sphereMesh);
-    //camera.lookAt(center);
-    //center.x -= 0.03;
-    //center.y += 0;
-    //scanPoints = new THREE.Points(scan.geometry, pointsMaterial)
-    //scan.material.color = 0x00ff00;
-    scene.add(sphereMesh);
-    scene.add(mesh);
-    console.log(mesh);
-    //scene.children[1].material.blending = THREE.AdditiveBlending;
-    //console.log(scan.geometry.attributes.position.array);
+  const scan = new THREE.Points(mesh.geometry, material);
+  //material.castShadows = true;
+  scan.material.color.setHSL(0.7, 0.5, 0.3);
+  scan.material.blending = THREE.AdditiveBlending;
+  mesh.position.y -= 20;
+  console.log(scan);
+  //scene.add(scan);
+  var box = new THREE.Box3().setFromObject(scan);
+  box.center(scan.position); // this re-sets the mesh position
+  scan.position.multiplyScalar(-1);
+
+  pivot = new THREE.Group();
+  scene.add(pivot);
+  pivot.add(scan);
+  pivot.rotation.x = Math.PI / 2;
+  pivot.position.y -= 15;
+  //pivot.position.x = 25;
+  console.log(pivot);
 });
 
-camera.position.z = 40;
+//camera.position.z = 25;
+//camera.position.x += 15;
 
 var animate = function () {
-    requestAnimationFrame(animate);
-
-    //mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  ballMesh.position.x = pointLight.position.x;
+  ballMesh.position.y = pointLight.position.y;
+  ballMesh.position.z = pointLight.position.z;
+  pivot.rotation.z += 0.002;
+  //mesh.rotation.x += 0.01;
+  //mesh.rotation.z += 0.01;
+  //scan.rotation.z += 0.001;
+  renderer.render(scene, camera);
 };
 
 animate();
